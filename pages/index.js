f369840107cb618125800c2d3f563a235f8e4c56
Natália Santana <nataliahie225@gmail.com>
2022-11-13 22:38:02 -0300

    Adicionando estilo ao HomePage

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,27 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <>
-      <h1 className='teste'>OLÁ, BEM VINDO AO NEWESTETICA</h1>
-      
 
-      
+      <nav>
+
+      </nav>
+      <header className={styles.headerHomepage}>
+        <p>NEWESTETICA</p>
+
+      </header>
+
+      <div className={styles.contentHomePage}>
+
+        <h3 className='teste'>OLÁ, BEM VINDO AO NEWESTETICA!</h3>
+
+        <div className='pt-3'>
+          <button type="button" className="btn btn-outline-primary m-2"> Cadastro  </button>
+          <button type="button" className="btn btn-outline-primary">Procedimentos</button>
+        </div>
+
+      </div>
+
+
     </>
   )
 }
